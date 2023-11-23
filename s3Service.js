cb60154398d02bd03900c4788d7e11e829371db5
James Adams <jamesadams@Jamess-MacBook-Air.local>
2023-11-24 08:50:52 +0900

    update admin pannel

--- s3Service.js
+++ s3Service.js
@@ -3,7 +3,6 @@ const { S3} = require("aws-sdk");
 const uuid = require("uuid")
 
 exports.s3Uploadv2 = async (file) => {
-
 	const s3 = new S3();
 	const param = {
 		Bucket: process.env.AWS_BUCKET_NAME,
